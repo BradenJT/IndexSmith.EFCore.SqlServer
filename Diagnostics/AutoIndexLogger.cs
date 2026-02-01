13016a82a7e5e6504a0f2791fa799d9b3c36bf84
Braden Townsell <bradenjparrish@gmail.com>
2026-02-01 14:28:47 -0600

    Add project files.

---
+++ Diagnostics/AutoIndexLogger.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexSmith.EFCore.SqlServer.Diagnostics
+{
+    internal class AutoIndexLogger
+    {
+    }
+}
---
+++ Heuristics/IndexScore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexSmith.EFCore.SqlServer.Heuristics
+{
+    internal class IndexScore
+    {
+    }
+}
